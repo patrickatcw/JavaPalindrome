b177fe372b9ca8f76cca1bb9412e1af2d0f269d8
patrickatcw <pj8392@yahoo.com>
2018-04-21 13:12:45 -0400

    set up initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,20 @@
+package com.me;
+
+/*
+directions;
+
+- create a program where the user inputs the number of palindromes they would like would like to see returned,
+then return that physical number of palindromes in the output.
+ex. if the user puts in the number "3", then the output would be;
+0, 1, 2
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
