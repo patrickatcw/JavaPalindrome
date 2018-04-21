d03ca316c4f9e5c4a26f41cab83fef95f3a39ca8
patrickatcw <pj8392@yahoo.com>
2018-04-21 13:36:53 -0400

    start of making program, need to validate numeric input, example commented, work on next

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,5 +1,7 @@
 package com.me;
 
+import java.util.Scanner;
+
 /*
 directions;
 
@@ -14,7 +16,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner userInputScanner = new Scanner(System.in);
+
+        System.out.println("\nPlease put in the number of palindromes you would like to see returned, then press enter;");
+        int input = Integer.parseInt(userInputScanner.nextLine());
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Please enter a positive number!");
+            while (!sc.hasNextInt()) {
+                System.out.println("That's not a number!");
+                sc.next(); // this is important!
+                    }
+            number = sc.nextInt();
+                } while (number <= 0);
+        System.out.println("Thank you! Got " + number);
+         */
+
+
+
     }
-
-
 }
