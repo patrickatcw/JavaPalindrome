69a590871d2a0619f5dd52d7561a11e8475681e0
patrickatcw <pj8392@yahoo.com>
2018-04-21 18:04:32 -0400

    finished method for validating number, now work on validating palindrome

---
+++ src/com/me/IsNumber.java
@@ -0,0 +1,29 @@
+package com.me;
+
+import java.util.Scanner;
+
+
+public class IsNumber {
+
+    public static void isActualNumber(){
+
+        Scanner sc = new Scanner(System.in);
+        int inputNumber;
+
+        do {
+            System.out.println("Please put in the number of palindromes you would like to see returned, " +
+                    "then press enter;");
+
+            while (!sc.hasNextInt()) {
+                System.out.println("That is not a number you HILLBILLY, now try again;");
+                sc.next(); // this is important!
+            }
+            inputNumber = sc.nextInt();
+        } while (inputNumber <= 0);
+        System.out.println("Thank you! Got " + inputNumber);
+        System.out.println("Here are the first " + inputNumber + " number(s)" + "that are palindromes....");
+
+    }
+
+}
+
--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,7 +1,5 @@
 package com.me;
 
-import java.util.Scanner;
-
 /*
 directions;
 
@@ -16,26 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner userInputScanner = new Scanner(System.in);
-
-        System.out.println("\nPlease put in the number of palindromes you would like to see returned, then press enter;");
-        int input = Integer.parseInt(userInputScanner.nextLine());
-
-        /*
-        Scanner sc = new Scanner(System.in);
-        int number;
-        do {
-            System.out.println("Please enter a positive number!");
-            while (!sc.hasNextInt()) {
-                System.out.println("That's not a number!");
-                sc.next(); // this is important!
-                    }
-            number = sc.nextInt();
-                } while (number <= 0);
-        System.out.println("Thank you! Got " + number);
-         */
-
-
+        IsNumber.isActualNumber();
 
     }
 }
